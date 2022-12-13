fe91101418651717d66f6b4eefea82ce58ad4c1c
Mariia_Khrustaleva(EPAM) <Mariia_Khrustaleva@epam.com>
2022-12-13 22:17:48 +0300

    homework 1. task 1 and 2

---
+++ tasks/task1.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+});
+
+rl.setPrompt('Please insert string: \n');
+rl.prompt();
+rl.on('line', (s) => {
+    console.log('Reversed string:');
+    console.log(`${[...s].reverse().join("")} \n`);
+
+    rl.prompt();
+})
---
+++ tasks/task2.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const csv = require('csvtojson')
+
+const inputFileURL = './csv/nodejs-hw1-ex1.csv';
+const outputFileURL = 'nodejs-hw1-ex1.txt';
+
+const readable = fs.createReadStream(inputFileURL, 'utf8');
+const writable = fs.createWriteStream(outputFileURL);
+let lineCount = 0;
+
+const onError = (err, module = 'parsing') => {
+    console.log(module ? `Error while ${module}: ${err}` : err);
+}
+const onComplete = () => {
+    console.log('Transformation completed');
+    readable.destroy();
+}
+writable
+    .on('error', (err) => {
+    onError(err, 'writing');
+})
+    .on('close', () => {
+    console.log('Writable stream is closed');
+})
+readable
+    .on('error', (err) => {
+    onError(err, 'reading');
+})
+    .on('end', () => {
+    console.log('Readable stream is closed');
+})
+csv()
+    .fromStream(readable)
+    .subscribe((json, lineNumber) => {
+        lineCount = lineNumber;
+        console.log(`Reading line ${lineNumber+1}...`);
+
+        writable.write(`${JSON.stringify(json)}\n`, () => {
+                console.log(`Writing line ${lineNumber+1}...`);
+                if(readable.readableEnded && lineNumber === lineCount) {
+                    writable.destroy();
+            }
+            });
+    },onError,onComplete);
